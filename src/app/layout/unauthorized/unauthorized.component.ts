283e8800c295cb97b3e488f6be4190315782c0a4
davitpavlenishvili <datunapavlenishvili@gmail.com>
2022-09-05 01:33:49 +0400

    refactor: reformatted code

--- src/app/layout/unauthorized/unauthorized.component.ts
+++ src/app/layout/unauthorized/unauthorized.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-unauthorized',
-  template: ` <div class="unauthorizedLayout">
-    <router-outlet></router-outlet>
-  </div>`,
+  template: `
+    <div class="unauthorizedLayout">
+      <router-outlet></router-outlet>
+    </div>`,
 })
 export class UnauthorizedComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
